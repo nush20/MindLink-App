fbfd8e94a8cf985996de99e84c856c0ed3fc9f39
nush20 <anushka.nd02@gmail.com>
2025-03-20 20:40:47 +0530

    Initial Commit

---
+++ MindLink App/MindLink_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindLink_AppApp.swift
+//  MindLink App
+//
+//  Created by Anushka on 20/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct MindLink_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
